2817b2b13bba76c8ea2045e6578f3ee0ea474e2e
sanqianll <2240017707@qq.com>
2021-04-04 23:46:44 +0800

    add new files

---
+++ 47_javascript/index.js
@@ -0,0 +1,11 @@
+var myname = prompt('请输入你的用户名');
+alert('欢迎' + myname);
+
+var a1=1;
+console.log(a1);
+var a1=1,
+    a2=2,
+    a3=3,//一次性定义多个变量
+    a4;//这是一个未赋值的变量
+console.log(a4);//undefined未定义
+//console.log(a5)//这行代码会报错。报错之后不会向后执行
