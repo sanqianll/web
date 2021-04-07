7521ceabb80b246f9e464895fa00897c0dec6c21
sanqianll <2240017707@qq.com>
2021-04-07 23:27:49 +0800

    add new files

---
+++ 50_js计算/index.js
@@ -0,0 +1,17 @@
+var first1 = 10;
+console.log(++first1 + 10);
+console.log(18 == '18');
+
+var num1 = 10; //赋值
+var num2 = 100; //赋值
+var res1 = num1 > num2; //false
+var res2 = num1 == 11; //false
+var res3 = num1 != num2; //true
+console.log(res1);
+console.log(res2);
+console.log(res3);
+
+console.log('--------------------------------------');
+console.log(123 && 456);
+console.log(123 && 456 && 222);
+console.log(0 && 456);
