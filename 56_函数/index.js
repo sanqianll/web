9c62437ab89abff6b3fa492b223aafc820b93e1d
sanqianll <2240017707@qq.com>
2021-04-16 01:34:19 +0800

    update 56_函数/index.js.

--- 56_函数/index.js
+++ 56_函数/index.js
@@ -44,5 +44,48 @@ function getArrMax(arr) {
     return max;
 }
 
-var arr=[1,2,3,3,4,5,6,7];
+var arr = [1, 2, 3, 3, 4, 5, 6, 7];
 console.log(getArrMax(arr));
+
+//-------------------------------------------------------
+//判断是否是素数，写入函数
+console.log('-----');
+
+function check_ss(a) {
+    var num = [];
+    for (var i = 0; i < a.length - 1; i++) {
+        for (var j = 2; j <= a[i]; j++) {
+            if (j == a[i]) {
+                num.push(a[i]);
+                break;
+            }
+            if (a[i] % j == 0) {
+                break;
+            }
+        }
+    }
+    return num;
+}
+var chek_num = check_ss([1, 2, 3, 4, 5, 6, 7, 78, 98, 9, 9]);
+var chek_num_str = chek_num.join();
+console.log(chek_num_str);
+
+//--------------------------------------------------------------
+//判断是否为闰年的函数
+function isRun(number) {
+    if (number % 4 == 0 && number % 100 != 0 || number % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function backDay() {
+    var years=prompt('请输入一个年份');
+    if (isRun(years)) {
+        alert('是闰年，二月有29天');
+    } else {
+        alert('是平年，二月有28天');
+    }
+}
+backDay();
