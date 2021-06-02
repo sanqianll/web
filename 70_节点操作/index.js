2856fa2f3dfd2bc5afa1679430b8aba9f7ed413d
sanqianll <2240017707@qq.com>
2021-06-03 00:25:38 +0800

    update 70_节点操作/index.js.

--- 70_节点操作/index.js
+++ 70_节点操作/index.js
@@ -1,3 +1,18 @@
 var con1 = document.querySelector('.con1');
 var box1 = con1.parentNode.parentNode; //写两个父级节点就是二级父节点
 console.log(box1);
+
+var ul = document.querySelector('.testul');
+
+// 使用childNode
+console.log(ul.childNodes);
+// 使用children
+console.log(ul.children);
+// 第一个子节点
+console.log(ul.firstChild);
+console.log(ul.firstElementChild);
+console.log(ul.children[0]);
+// 最后一个子节点
+console.log(ul.lastChild);
+console.log(ul.lastElementChild);
+console.log(ul.children[ul.children.length - 1]);
