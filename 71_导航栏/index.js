96331c705c00999cd6a95cfc8f4bb77f60449b31
sanqianll <2240017707@qq.com>
2021-06-13 23:54:26 +0800

    update 71_导航栏/index.js.

--- 71_导航栏/index.js
+++ 71_导航栏/index.js
@@ -2,6 +2,7 @@ var nav = document.querySelector('.nav');
 var lis = nav.children; //得到四个li
 for (var i = 0; i < lis.length; i++) {
     lis[i].onmouseover = function () {
+        // this.children.length-1 为li 中最后一个元素 指ul
         this.children[this.children.length - 1].style.display = 'block'
     }
     lis[i].onmouseout = function () {
