843b081642826347ac45c425d9a60adfd818ef39
sanqianll <2240017707@qq.com>
2021-06-27 08:56:01 +0000

    update 72_留言板/index.js.

--- 72_留言板/index.js
+++ 72_留言板/index.js
@@ -1,16 +1,43 @@
 var textarea = document.querySelector('textarea');
-var btn = document.querySelector('button');
+var btn = document.querySelector('.push');
+var del = document.querySelector('.del');
 var ul = document.querySelector('ul');
+
+// 发布按钮
 btn.onclick = function () {
     if (textarea.value == '') {
         alert("评论不能为空。");
     } else {
         var li = document.createElement('li');
         var addtime = document.createElement('span');
-        li.innerHTML = textarea.value;
+        li.innerHTML = textarea.value + '<a href="javascript:;" class="adda">删除</a>';
         addtime.innerHTML = "评论时间：" + gettime();
         ul.insertBefore(li, ul.children[0]);
         ul.insertBefore(addtime, ul.children[1]);
+        del.disabled = false;
+        var as = document.querySelectorAll('.adda');
+        for (var i = 0; i < as.length; i++) {
+            as[i].onclick = function () {
+                ul.removeChild(this.parentNode.nextSibling);
+                ul.removeChild(this.parentNode);
+            }
+        }
+    }
+}
+// 删除按钮
+del.onclick = function () {
+    if (ul.children.length == 0) {
+        this.disabled = true;
+        // this.style.cursur = 'not - allowed';
+    } else {
+        ul.removeChild(ul.children[0]);
+        ul.removeChild(ul.children[0]);
+        if (ul.children.length == 0) {
+            this.disabled = true;
+            // this.style.cursur = 'not - allowed';
+        } else {
+            this.disabled = false;
+        }
     }
 }
 
